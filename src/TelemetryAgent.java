9a756ed1ef9e6f0323be9ad54a7063f206c96e05
Alberto Costa <73739928+acostaRossi@users.noreply.github.com>
2026-02-10 13:12:49 +0100

    Aggiunta gestione multithreading

--- src/TelemetryAgent.java
+++ src/TelemetryAgent.java
@@ -1,3 +1,7 @@
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.GlobalMemory;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -7,17 +11,30 @@ public class TelemetryAgent {
 
     public static void main(String[] args) {
 
-        try {
-            for(int i = 0; i < 10000; i++) {
-                String s = "Ciao Server sono l'Agent " + i;
+        while(true) {
+            try {
+                SystemInfo si = new SystemInfo();
+                GlobalMemory gmemory = si.getHardware().getMemory();
+                long totalMemory = gmemory.getTotal();
+                long usedMemory = totalMemory - gmemory.getAvailable();
+                String os = si.getOperatingSystem().getFamily();
+                long time = System.currentTimeMillis();
+                String s = time + ";" + totalMemory + ";" + usedMemory + ";" + os;
                 byte[] buffer = s.getBytes();
                 InetAddress address = InetAddress.getByName("127.0.0.1");
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 9000);
                 DatagramSocket socket = new DatagramSocket();
                 socket.send(packet);
+
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
--- src/TelemetryServer.java
+++ src/TelemetryServer.java
@@ -1,10 +1,14 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.Socket;
 import java.net.SocketException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class TelemetryServer {
-
 
     public static void main(String[] args) {
 
@@ -16,15 +20,22 @@ public class TelemetryServer {
                 byte[] buffer = new byte[1024];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 socket.receive(packet); // bloccante
-                String s = new String(packet.getData(), 0, packet.getLength());
-                System.out.println(s);
+                new Thread(() -> handleClient(packet)).start();
             }
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 
-
+    public static void handleClient(DatagramPacket packet) {
+        String s = new String(packet.getData(), 0, packet.getLength());
+        String[] parts = s.split(";");
+        long time = Long.parseLong(parts[0]);
+        long totalMemory = Long.parseLong(parts[1]);
+        long usedMemory = Long.parseLong(parts[2]);
+        String os = parts[3];
+        LocalDateTime lDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(time), ZoneId.systemDefault());
+        String sFinal = lDateTime.format(DateTimeFormatter.ISO_DATE_TIME) + " " + totalMemory + " " + usedMemory + " " + os;
+        System.out.println(sFinal);
+    }
 }
